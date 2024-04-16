b903185b95377bbf0e22f5e74977b1c7b7786f67
urel <urielpedroso91@gmail.com>
2024-04-15 23:37:53 -0300

    Add complete order feature

--- src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
+++ src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
@@ -1,8 +1,10 @@
+import { CreditCard } from "phosphor-react";
 import { PaymentMethodContainer } from "./styles";
 
 export function PaymentMethodInput(){
     return(
         <PaymentMethodContainer>
+            <CreditCard size={16} />
             Cartao de caralho
         </PaymentMethodContainer>
     )
